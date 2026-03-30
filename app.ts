31c6754388eff4522c8b324f86a1ea10dfcf9788
bb7talla@gmail.com <bhaskar@bhaskars-MacBook-Air.local>
2026-03-30 14:22:39 +0530

    Initial commit

---
+++ app.ts
@@ -0,0 +1,32 @@
+import type { Request, Response, NextFunction } from 'express'
+import express from 'express'
+import morgan from 'morgan'
+
+import { router as tourRouter } from './routes/tourRoutes'
+import { router as userRouter } from './routes/userRoutes'
+
+const app = express()
+
+// 1) MIDDLEWARES
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
+app.use(express.json())
+app.use(express.static(`${__dirname}/public`))
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  console.log('Hello from the middleware 👋')
+  next()
+})
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  req.requestTime = new Date().toISOString()
+  next()
+})
+
+// 3) ROUTES
+app.use('/api/v1/tours', tourRouter)
+app.use('/api/v1/users', userRouter)
+
+export default app
---
+++ routes/tourRoutes.ts
@@ -0,0 +1,20 @@
+import express from 'express'
+import {
+  checkID,
+  getAllTours,
+  checkBody,
+  createTour,
+  getTour,
+  updateTour,
+  deleteTour,
+} from './../controllers/tourController'
+
+const router = express.Router()
+
+router.param('id', checkID)
+
+router.route('/').get(getAllTours).post(checkBody, createTour)
+
+router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour)
+
+export { router }
