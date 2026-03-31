ed71e7800c52108c9b728350cfd892795bbfa52d
bb7talla@gmail.com <bhaskar@bhaskars-MacBook-Air.local>
2026-03-31 12:12:16 +0530

    Installed mongoose and connected to database

--- server.ts
+++ server.ts
@@ -1,8 +1,18 @@
+import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 
 dotenv.config({ path: './config.env' })
 
 import app from './app'
+
+const DB = process.env.DATABASE?.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD ?? '',
+)
+
+mongoose.connect(DB as string).then((con) => {
+  console.log('🚀 ~ Databse connected successfully:')
+})
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
